ca46e7019d1a583661f6449ac19fcf279e521e99
THOMVDC <thomvdc@gmail.com>
2023-02-24 16:31:11 +0100

    Deleted files from Git repository and filesystem

--- LTNP_is_this_same_image.m
+++
@@ -1,9 +0,0 @@
-function [answer]=LTNP_is_this_same_image(image1,image2)
-[I1,Vref1]=LCN12_read_image(image1);
-[I2,Vref2]=LCN12_read_image(image2);
-
-if isequal(I1,I2) && isequal(Vref1,Vref2)
-    answer=true(1);
-else
-    answer=false(1);
-end
